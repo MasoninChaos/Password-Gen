5f97f9ba7f9add06ca7bd3a66d4398efd1b280e7
Mason Wright <Mason.Wright@rc.local>
2024-12-04 14:46:27 +0000

    First Commit

---
+++ Untitled-1.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+MyOrders = "Mason's Orders:"
+Pyhton = "Hello World, YOU MUST SAY HELLO BACK" 
+peepeepoopoo = [1,2,3,4,5,6,7,8,9,10,]
+print(MyOrders)
+print (Pyhton) 
+method = input("Is the user on the phone? (Y/N)")
+if not Pyhton != "Hello World":
+    print("Hate you world") 
+else:
+        print("I might be the first person to eat the lead cups at mcdonalds but I wont be the last") 
+
+
+print("I chew flux core solder for the taste not the texture") 
+print("I\'m at your mommas house smoking that buffon pack") 
+print("chewing the little bits of rubber of my shoe and spitting them into a canteen like a cowboy")
+print("listen someone is giving it up its either you or the dragon and I aint afriad of a little burn")
+
+for number in peepeepoopoo:
+      print(number)
+for i in range(1,len(peepeepoopoo)):
+      print(peepeepoopoo[i])
+
+def main():
+      pass
+
+
+if __name__ == "__main__": 
+      main()
---
+++ password.py
@@ -0,0 +1,24 @@
+import random
+import pyperclip
+method = input("Is the user on the phone? (Y/N)")
+alphabet = ['a', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+alphabetNato = ['Alpha', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'Indigo', 'Juliet', 'Lima', 'Mike', 'Oscar',  'Quebec', 'Romeo', 'Sierra', 'Tango', 'Victor', 'Whiskey', 'Xray', 'Yankee', 'Zulu']
+SpecialA = ['!', '£', '$','*']
+SpecialANato = ['Exclamation Mark', 'Pound Sign', 'Dollar Sign','Asterisk']
+capitals = ['A', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+NumbersNato = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+randomA = random.choices(population= alphabet, k=7)
+RandomB = random.choices(population= SpecialA, k=1) 
+RandomC = random.choices(population= capitals, k=1)
+RandomD = random.choices(population=  numbers, k=2)
+passwordtest = (randomA + RandomB + RandomC + RandomD)
+
+print(''.join(map(str, passwordtest))) 
+
+pyperclip.copy(''.join(map(str, passwordtest)))
+pyperclip.paste()
+passwordtest
+
+
+
